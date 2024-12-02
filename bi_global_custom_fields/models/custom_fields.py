c922595851b2599c575dc6152663929f2845edf7
Sherif-Arnaout <165007513+Sherif-Arnaout@users.noreply.github.com>
2024-12-02 23:07:03 +0200

    Done

--- bi_global_custom_fields/models/custom_fields.py
+++ bi_global_custom_fields/models/custom_fields.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+# Parts of BrowseInfo. See LICENSE file for full copyright and licensing details.
 
 from subprocess import call
 from lxml import etree
